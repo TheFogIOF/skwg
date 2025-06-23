6fa88a05288abe79d76034c0bf5091fdef447a19
TheFogIOF <70043840+TheFogIOF@users.noreply.github.com>
2025-06-24 04:59:56 +1000

    Add files via upload

---
+++ skwg/general_zapret.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001 > nul
+:: 65001 - UTF-8
+
+cd /d "%~dp0"
+echo:
+
+set "BIN=%~dp0"
+set "LISTS=%~dp0lists\"
+
+start "TheFogIOF's config" /min "%BIN%winws.exe" --wf-tcp=80,443 --wf-udp=443,50000-50100 ^
+--filter-udp=443 --hostlist="%LISTS%list-twitch.txt" --dpi-desync=fake --dpi-desync-repeats=3 --dpi-desync-fooling=badseq,hopbyhop2 --new ^
+--filter-tcp=443 --hostlist="%LISTS%list-twitch.txt" --dpi-desync=fake --dpi-desync-fooling=badseq --new ^
+--filter-udp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,split --dpi-desync-repeats=6 --dpi-desync-fake-quic="%BIN%quic_initial_www_google_com.bin" --new ^
+--filter-udp=50000-50100 --filter-l7=discord,stun --dpi-desync=fake --dpi-desync-cutoff=d3 --dpi-desync-repeats=6 --new ^
+--filter-tcp=80 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,split2 --dpi-desync-autottl=2 --dpi-desync-fooling=badseq --new ^
+--filter-tcp=443 --hostlist="%LISTS%list-general.txt" --dpi-desync=fake,split --dpi-desync-fooling=badseq --dpi-desync-split-seqovl-pattern="%BIN%tls_clienthello_www_google_com.bin" --dpi-desync-fake-tls-mod=sni=www.google.com
+exit
